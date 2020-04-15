f71513e663fb78db03028a87a533224d56a851f9
antongomez <anton.gomez.lopez@rai.usc.es>
2020-04-15 12:33:15 +0200

    Configuracion Inicial

--- gui/FachadaGui.java
+++ gui/FachadaGui.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Esther
- */
 public class FachadaGui {
-    
+
     aeropuerto.FachadaAplicacion fa;
 
     public FachadaGui(aeropuerto.FachadaAplicacion fa) {
@@ -24,7 +14,7 @@ public class FachadaGui {
     }
 
     public void iniciarVista(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/gui/vista/vAcceder.fxml"));
+        Parent root = FXMLLoader.load(FachadaGui.class.getResource("/gui/vista/vAcceder.fxml"));
         primaryStage.setTitle("Aeroporto");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
