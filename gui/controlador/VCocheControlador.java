f9a3df882a8bc3e8c41c4b91f1ca743811055912
elipitav <57717880+elipitav@users.noreply.github.com>
2020-05-04 19:45:54 +0200

    Conflictos

--- gui/controlador/VCocheControlador.java
+++ gui/controlador/VCocheControlador.java
@@ -7,7 +7,15 @@ package gui.controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -15,13 +23,45 @@ import javafx.fxml.Initializable;
  * @author eliseopitavilarino
  */
 public class VCocheControlador implements Initializable {
+    
+    //Apartados con reserva
+    @FXML
+    private TextField textFieldDNIConReserva;
+    @FXML
+    private Button btnBuscarConReserva;
+    @FXML
+    private TableView<?> tablaConReservatablaConReserva;
+    @FXML
+    private Button btnAlquilarConReserva;
+    @FXML
+    private RadioButton cambiarFechaConReserva;
+    @FXML
+    private DatePicker datePickerConReserva;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        cambiarFechaConReserva.setSelected(false);
+        datePickerConReserva.setDisable(true);
+        btnAlquilarConReserva.setDisable(true);
     }    
+
+    @FXML
+    private void obtenerReservasUsuario(ActionEvent event) {
+    }
+
+    @FXML
+    private void seleccionarConReserva(MouseEvent event) {
+    }
+
+    @FXML
+    private void alquilarConReserva(ActionEvent event) {
+    }
+
+    @FXML
+    private void activarDatePicker(ActionEvent event) {
+    }
     
 }
